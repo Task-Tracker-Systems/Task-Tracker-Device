80536d48d90845b3ee52cc029845c45e4533b0a7
David Hebbeker <david@hebbeker.info>
2022-05-30 21:46:10 +0200

    Give notes a type.

--- time-tracker-src/src/pitches.hpp
+++ time-tracker-src/src/pitches.hpp
@@ -1,89 +1,96 @@
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
+#pragma once
+
+#include <cstdint>
+
+namespace note
+{
+constexpr std::uint16_t B0 = 31;
+constexpr std::uint16_t C1 = 33;
+constexpr std::uint16_t CS1 = 35;
+constexpr std::uint16_t D1 = 37;
+constexpr std::uint16_t DS1 = 39;
+constexpr std::uint16_t E1 = 41;
+constexpr std::uint16_t F1 = 44;
+constexpr std::uint16_t FS1 = 46;
+constexpr std::uint16_t G1 = 49;
+constexpr std::uint16_t GS1 = 52;
+constexpr std::uint16_t A1 = 55;
+constexpr std::uint16_t AS1 = 58;
+constexpr std::uint16_t B1 = 62;
+constexpr std::uint16_t C2 = 65;
+constexpr std::uint16_t CS2 = 69;
+constexpr std::uint16_t D2 = 73;
+constexpr std::uint16_t DS2 = 78;
+constexpr std::uint16_t E2 = 82;
+constexpr std::uint16_t F2 = 87;
+constexpr std::uint16_t FS2 = 93;
+constexpr std::uint16_t G2 = 98;
+constexpr std::uint16_t GS2 = 104;
+constexpr std::uint16_t A2 = 110;
+constexpr std::uint16_t AS2 = 117;
+constexpr std::uint16_t B2 = 123;
+constexpr std::uint16_t C3 = 131;
+constexpr std::uint16_t CS3 = 139;
+constexpr std::uint16_t D3 = 147;
+constexpr std::uint16_t DS3 = 156;
+constexpr std::uint16_t E3 = 165;
+constexpr std::uint16_t F3 = 175;
+constexpr std::uint16_t FS3 = 185;
+constexpr std::uint16_t G3 = 196;
+constexpr std::uint16_t GS3 = 208;
+constexpr std::uint16_t A3 = 220;
+constexpr std::uint16_t AS3 = 233;
+constexpr std::uint16_t B3 = 247;
+constexpr std::uint16_t C4 = 262;
+constexpr std::uint16_t CS4 = 277;
+constexpr std::uint16_t D4 = 294;
+constexpr std::uint16_t DS4 = 311;
+constexpr std::uint16_t E4 = 330;
+constexpr std::uint16_t F4 = 349;
+constexpr std::uint16_t FS4 = 370;
+constexpr std::uint16_t G4 = 392;
+constexpr std::uint16_t GS4 = 415;
+constexpr std::uint16_t A4 = 440;
+constexpr std::uint16_t AS4 = 466;
+constexpr std::uint16_t B4 = 494;
+constexpr std::uint16_t C5 = 523;
+constexpr std::uint16_t CS5 = 554;
+constexpr std::uint16_t D5 = 587;
+constexpr std::uint16_t DS5 = 622;
+constexpr std::uint16_t E5 = 659;
+constexpr std::uint16_t F5 = 698;
+constexpr std::uint16_t FS5 = 740;
+constexpr std::uint16_t G5 = 784;
+constexpr std::uint16_t GS5 = 831;
+constexpr std::uint16_t A5 = 880;
+constexpr std::uint16_t AS5 = 932;
+constexpr std::uint16_t B5 = 988;
+constexpr std::uint16_t C6 = 1047;
+constexpr std::uint16_t CS6 = 1109;
+constexpr std::uint16_t D6 = 1175;
+constexpr std::uint16_t DS6 = 1245;
+constexpr std::uint16_t E6 = 1319;
+constexpr std::uint16_t F6 = 1397;
+constexpr std::uint16_t FS6 = 1480;
+constexpr std::uint16_t G6 = 1568;
+constexpr std::uint16_t GS6 = 1661;
+constexpr std::uint16_t A6 = 1760;
+constexpr std::uint16_t AS6 = 1865;
+constexpr std::uint16_t B6 = 1976;
+constexpr std::uint16_t C7 = 2093;
+constexpr std::uint16_t CS7 = 2217;
+constexpr std::uint16_t D7 = 2349;
+constexpr std::uint16_t DS7 = 2489;
+constexpr std::uint16_t E7 = 2637;
+constexpr std::uint16_t F7 = 2794;
+constexpr std::uint16_t FS7 = 2960;
+constexpr std::uint16_t G7 = 3136;
+constexpr std::uint16_t GS7 = 3322;
+constexpr std::uint16_t A7 = 3520;
+constexpr std::uint16_t AS7 = 3729;
+constexpr std::uint16_t B7 = 3951;
+constexpr std::uint16_t C8 = 4186;
+constexpr std::uint16_t CS8 = 4435;
+constexpr std::uint16_t D8 = 4699;
+constexpr std::uint16_t DS8 = 4978;
+}
