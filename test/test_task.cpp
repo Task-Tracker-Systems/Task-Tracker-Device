129ac319a649359d198df8f526c4e8773ba7a506
David Hebbeker <david@hebbeker.info>
2023-04-27 21:57:56 +0200

    Add test case for start/stop.

--- test/test_task.cpp
+++ test/test_task.cpp
@@ -1,5 +1,10 @@
 #include "Task.hpp"
+#include <ArduinoFake.h>
 #include <unity.h>
+
+static const Task::String label(L"äüöß");
+
+using namespace fakeit;
 
 void setUp()
 {
@@ -11,9 +16,19 @@ void tearDown()
 
 void test_get_label()
 {
-    const Task::String label(L"foo");
     const Task task(label);
     TEST_ASSERT_EQUAL_STRING(label.c_str(), task.getLabel().c_str());
+}
+
+void test_time_elapses()
+{
+    Task task(label);
+    TEST_ASSERT_EQUAL_UINT(0U, task.getRecordedDuration());
+    When(Method(ArduinoFake(), millis)).Return(0);
+    task.start();
+    When(Method(ArduinoFake(), millis)).Return(2000);
+    task.stop();
+    TEST_ASSERT_EQUAL_UINT(2U, task.getRecordedDuration());
 }
 
 int main(int argc, char **argv)
@@ -21,6 +36,7 @@ int main(int argc, char **argv)
     UNITY_BEGIN();
 
     RUN_TEST(test_get_label);
+    RUN_TEST(test_time_elapses);
 
     UNITY_END();
 }
