22f19c7a0fe38edfd793c78e849946e9a4ae5f0e
David Hebbeker <david@hebbeker.info>
2023-04-28 12:10:39 +0200

    Add status indicator implementation for output shift registers.
    (cherry picked from commit 59cb4d54ba4b0a054460c1a12e86c1d94e608854)

---
+++ lib/bal/ShiftRegisterStatusIndicator_implementation.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "ShiftRegisterStatusIndicator.hpp"
+#include <Arduino.h>
+
+template <class ShiftRegister>
+void ShiftRegisterStatusIndicator<ShiftRegister>::off()
+{
+    shiftRegister.set(pin, LOW);
+}
+
+template <class ShiftRegister>
+ShiftRegisterStatusIndicator<ShiftRegister>::ShiftRegisterStatusIndicator(const ShiftRegister &outputShiftRegister, const std::uint8_t pinToUse)
+    : shiftRegister(outputShiftRegister), pin(pinToUse)
+{
+}
+
+template <class ShiftRegister>
+void ShiftRegisterStatusIndicator<ShiftRegister>::on()
+{
+    shiftRegister.set(pin, HIGH);
+}
