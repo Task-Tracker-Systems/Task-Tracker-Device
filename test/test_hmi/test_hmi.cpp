8d6eedf23bc6c22731828083e8579d9b4a070f55
David Hebbeker <david@hebbeker.info>
2023-05-28 15:21:56 +0200

    Use mocks for testing Controller.

--- test/test_hmi/test_hmi.cpp
+++ test/test_hmi/test_hmi.cpp
@@ -1,6 +1,8 @@
 #include "Controller.hpp"
-#include "keypad_factory_interface.hpp"
+#include <ArduinoFake.h>
 #include <unity.h>
+
+using namespace fakeit;
 
 void setUp()
 {
@@ -12,12 +14,43 @@ void tearDown()
 
 void test_Controller()
 {
-    const auto &fakeKeypad = board::getKeypad();
-    Controller controller(fakeKeypad);
+    Mock<IKeypad> fakeKeypad;
+    Controller controller(fakeKeypad.get());
 
-    // First no key is pressed
-    const auto event_cand = controller.checkHmiInput();
-    TEST_ASSERT_EQUAL(false, event_cand.has_value());
+    {
+        // First no key is pressed
+        When(Method(fakeKeypad, getCurrentlyPressedKey)).Return(KeyId::NONE);
+        const auto event_cand = controller.checkHmiInput();
+        TEST_ASSERT_EQUAL(false, event_cand.has_value());
+    }
+
+    {
+        // Again no key is pressed
+        When(Method(fakeKeypad, getCurrentlyPressedKey)).Return(KeyId::NONE);
+        const auto event_cand = controller.checkHmiInput();
+        TEST_ASSERT_EQUAL(false, event_cand.has_value());
+    }
+
+    {
+        // Now some key is pressed
+        When(Method(fakeKeypad, getCurrentlyPressedKey)).Return(KeyId::ENTER);
+        const auto event_cand = controller.checkHmiInput();
+        TEST_ASSERT_EQUAL(false, event_cand.has_value());
+    }
+
+    {
+        // That key is not released yet
+        When(Method(fakeKeypad, getCurrentlyPressedKey)).Return(KeyId::ENTER);
+        const auto event_cand = controller.checkHmiInput();
+        TEST_ASSERT_EQUAL(false, event_cand.has_value());
+    }
+
+    {
+        // That key is not released yet
+        When(Method(fakeKeypad, getCurrentlyPressedKey)).Return(KeyId::NONE);
+        const auto event_cand = controller.checkHmiInput();
+        TEST_ASSERT_EQUAL(KeyId::ENTER, event_cand.value());
+    }
 }
 
 int main(int argc, char **argv)
