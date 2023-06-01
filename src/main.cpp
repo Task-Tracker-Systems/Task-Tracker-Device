6ae5de249e72cbd07816551b7c1f6a6b64f8b9e3
David Hebbeker <david@hebbeker.info>
2023-06-01 22:06:01 +0200

    Refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "main.hpp"
+#include "Controller.hpp"
 #include "board_pins.hpp"
 #include "display.h"
+#include "keypad_factory_interface.hpp"
 #include "pitches.hpp"
 #include <Arduino.h>
-#include <RoxMux.h>
 #include <ShiftRegister74HC595.h>
 #include <cstdint>
 
@@ -12,45 +13,11 @@
  */
 ShiftRegister74HC595<1U> outputShiftRegister(board::osr::pin::data, board::osr::pin::clock, board::osr::pin::latch);
 
-/**
- * Input shift register.
- */
-Rox74HC165<1U> inputShiftRegister;
-
-/**
- * @returns 1-8 or 0 in case of no event
- */
-static std::uint8_t getEvent()
-{
-    static std::uint8_t oldValue = 0U;
-    std::uint8_t newValue = 0U;
-
-    inputShiftRegister.update();
-    const auto numberOfPins = inputShiftRegister.getLength();
-    for (std::uint8_t newValueCand = numberOfPins; newValueCand > 0; newValueCand--)
-    {
-        if (inputShiftRegister.readPin(numberOfPins - newValueCand))
-        {
-            newValue = newValueCand;
-            break;
-        }
-    }
-
-    std::uint8_t result = 0;
-    if (newValue != 0 && oldValue == 0)
-    {
-        result = newValue;
-    }
-    oldValue = newValue;
-    return result;
-}
-
 namespace main
 {
 void setup(char const *programIdentificationString)
 {
     pinMode(board::buzzer::pin::on_off, OUTPUT);
-    inputShiftRegister.begin(board::isr::pin::data, board::isr::pin::latch, board::isr::pin::clock);
     setup_display();
     Serial.begin(115200);
     delay(100);
@@ -60,14 +27,16 @@ void setup(char const *programIdentificationString)
 }
 void loop()
 {
-    const auto event = getEvent();
+    static const Controller controller(board::getKeypad());
+    const auto event = controller.checkHmiInput();
     if (event)
     {
-        Serial.printf("Process event '%u'.\n", event);
+        const std::uint8_t eventNumber = static_cast<std::uint8_t>(event.value()); //!< @TODO process ID instead of integer
+        Serial.printf("Process event '%u'.\n", eventNumber);
         constexpr std::uint16_t notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
-        const std::uint8_t newRegisterValue = 1 << (8 - event);
+        const std::uint8_t newRegisterValue = 1 << (8 - eventNumber);
         outputShiftRegister.setAll(&newRegisterValue);
-        tone(board::buzzer::pin::on_off, notes[event - 1], 250);
+        tone(board::buzzer::pin::on_off, notes[eventNumber - 1], 250);
     }
     else
     {
