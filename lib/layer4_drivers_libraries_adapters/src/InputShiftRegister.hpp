8ea299492414e23588253d2d344126e99aa7066f
David Hebbeker <david@hebbeker.info>
2023-06-01 22:04:26 +0200

    Fixed some coding issues in InputShiftRegister class.

--- lib/layer4_drivers_libraries_adapters/src/InputShiftRegister.hpp
+++ lib/layer4_drivers_libraries_adapters/src/InputShiftRegister.hpp
@@ -8,17 +8,19 @@ template <std::size_t numberOfBytes>
 class InputShiftRegister : public IInputShiftRegister<numberOfBytes * 8>
 {
   public:
+    //! Alias to the interface this class implements
+    using Base = IInputShiftRegister<numberOfBytes * 8>;
     typedef Rox74HC165<numberOfBytes> Backend;
     InputShiftRegister(Backend &isrImplementation)
         : isrBackend(isrImplementation)
     {
     }
 
-    virtual Data readRegister() const override
+    virtual typename Base::Data readRegister() const override
     {
-        Data data{};
+        typename Base::Data data{};
         isrBackend.update();
-        std::uint16_t pinIndex = numberOfBits - 1;
+        std::uint16_t pinIndex = Base::numberOfBits - 1;
         for (auto pin : data)
         {
             pin = isrBackend.readPin(pinIndex--);
@@ -27,5 +29,5 @@ class InputShiftRegister : public IInputShiftRegister<numberOfBytes * 8>
     }
 
   private:
-    mutable Backend &isrBackend;
+    Backend &isrBackend;
 };
