d40f74da32ae5957b262342ee07a641468b15e77
David Hebbeker <david@hebbeker.info>
2023-06-05 22:10:15 +0200

    Draw dependency from Presenter to IStatusIndicator.

--- lib/layer2_hmi/src/Presenter.hpp
+++ lib/layer2_hmi/src/Presenter.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include "IPresenter.hpp"
+#include "IStatusIndicator.hpp"
+#include <array>
 
 class Presenter : public IPresenter
 {
   public:
     virtual void setTaskStatusIndicator(const TaskIndex, const TaskIndicatorState) override;
+
+  private:
+    const std::array<IStatusIndicator *const, 4> statusIndicators;
 };
