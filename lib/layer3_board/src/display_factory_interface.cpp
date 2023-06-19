a67c5a2d10566466fb82815506bd6cb80e421d0c
David Hebbeker <david@hebbeker.info>
2023-06-19 11:31:35 +0200

    Implement factory for Display.

--- lib/layer3_board/src/display_factory_interface.cpp
+++ lib/layer3_board/src/display_factory_interface.cpp
@@ -1,8 +1,11 @@
 #include "display_factory_interface.hpp"
+#include "Display.hpp"
 
 namespace board
 {
 IDisplay &getDisplay()
 {
+    static Display singleton;
+    return singleton;
 }
 } // namespace board
