b15d1c66f6a9f15ea43c1113b92add38f36187d3
David Hebbeker <david@hebbeker.info>
2023-09-17 18:57:18 +0200

    Uncomment code which creates a circular dependency.

--- lib/layer2_hmi/src/Presenter.cpp
+++ lib/layer2_hmi/src/Presenter.cpp
@@ -1,6 +1,6 @@
 #include "Presenter.hpp"
-#include "board_config.hpp"
-#include "pitches.hpp"
+// #include "board_config.hpp"
+// #include "pitches.hpp"
 #include <Arduino.h>
 #include <cassert>
 
@@ -11,8 +11,8 @@ void Presenter::setTaskStatusIndicator(const TaskIndex index, const TaskIndicato
     {
     case TaskIndicatorState::ACTIVE:
         statusIndicators[index]->on();
-        constexpr auto notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
-        tone(board::buzzer::pin::on_off, notes[index - 1], 250);
+        // constexpr auto notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
+        // tone(board::buzzer::pin::on_off, notes[index - 1], 250);
         break;
     case TaskIndicatorState::INACTIVE:
         statusIndicators[index]->off();
@@ -26,5 +26,5 @@ void Presenter::setTaskStatusIndicator(const TaskIndex index, const TaskIndicato
 Presenter::Presenter(Menu &menuToUse, IStatusIndicator *const (&statusIndicatorsToUse)[hmi::numberOfStatusIndicators])
     : menu(menuToUse), statusIndicators(statusIndicatorsToUse)
 {
-    pinMode(board::buzzer::pin::on_off, OUTPUT);
+    // pinMode(board::buzzer::pin::on_off, OUTPUT);
 }
