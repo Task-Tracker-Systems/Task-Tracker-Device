302f531498a2606513e8ffc89a29530c7820c242
dhebbeker <dhebbeker@users.noreply.github.com>
2023-09-17 08:26:20 +0000

    deploy: 3651192c66619ee1932151510aa8e3bc33793fb9

---
+++ feature/add-place-to-document-adrs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['display_0',['display',['../display_8cpp.xhtml#ae780a76d06cb56e14435ebfe3db3f6d3',1,'display.cpp']]]
+];
---
+++ feature/add-place-to-document-adrs/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme_0',['readme',['../md_doc_2design-decisions_2README.xhtml',1,'README'],['../md_hardware_2README.xhtml',1,'README']]]
+];
