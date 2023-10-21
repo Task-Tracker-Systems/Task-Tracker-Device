0855f86b4bf1dade9103c3b78a33388e71d10d70
David Hebbeker <david@hebbeker.info>
2023-10-21 22:59:31 +0200

    Add example code for `serialEvent()`.
    Copied from
    https://github.com/dhebbeker/echo-device/blob/854a72840e42d53cf2f4d47449c9cf2603528956/src/main.cpp#L49
    
    (cherry picked from commit 6e146d8b7f1b96be0ddb72914195d5a062dcdefe)

--- lib/3rd_party_adapters/serial_port.cpp
+++ lib/3rd_party_adapters/serial_port.cpp
@@ -44,3 +44,31 @@ void subscribeToIncomingLine(StringHandler &callback)
 }
 
 } // namespace serial_port
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent()
+{
+    while (Serial.available() > 0)
+    {
+        const auto inData = Serial.read();
+        if (inData < 0)
+        {
+            break;
+        }
+        else
+        {
+            const auto inChar = static_cast<char>(inData);
+            inputString += inChar;
+            // if the incoming character is a newline, set a flag so the main loop can
+            // do something about it:
+            if (inChar == '\n' || inChar == '\r')
+            {
+                stringComplete = true;
+            }
+        }
+    }
+}
