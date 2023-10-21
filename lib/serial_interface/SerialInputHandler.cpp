97f48607df7eb66c1adb1ac0eb997c2bba0d4552
David Hebbeker <david@hebbeker.info>
2023-10-21 22:55:19 +0200

    Prepare serial input handler.
    (cherry picked from commit 7b90a9afb4a707ada531bc7bb08ac7f7df88fc0e)

--- lib/serial_interface/SerialInputHandler.cpp
+++ lib/serial_interface/SerialInputHandler.cpp
@@ -1,6 +1,11 @@
 #include "SerialInputHandler.hpp"
+#include <Arduino.h>
 
 SerialInputHandler::SerialInputHandler()
 {
     // TODO Auto-generated constructor stub
+}
+
+void SerialInputHandler::handleNewSerialData(Stream &stream)
+{
 }
--- lib/serial_interface/SerialInputHandler.hpp
+++ lib/serial_interface/SerialInputHandler.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <queue>
+#include <string>
+
+class Stream;
 
 class SerialInputHandler
 {
   public:
     SerialInputHandler();
+    void handleNewSerialData(Stream &stream);
+
+  private:
+    std::queue<std::string> messageQueue;
 };
