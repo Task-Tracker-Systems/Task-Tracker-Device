e40cc1a1c2e7fe3a77fcc22052cbe8eb2ecc5986
dhebbeker <dhebbeker@users.noreply.github.com>
2023-10-29 10:19:34 +0000

    deploy: a975477f08ea714dcdac087d42efeda9029aa08f

--- develop/search/all_d.js
+++ develop/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['readme_0',['README',['../md_doc_design_decisions_README.xhtml',1,'(Global Namespace)'],['../md_hardware_README.xhtml',1,'(Global Namespace)']]],
-  ['readme_2emd_1',['README.md',['../doc_2design-decisions_2README_8md.xhtml',1,'(Global Namespace)'],['../hardware_2README_8md.xhtml',1,'(Global Namespace)'],['../README_8md.xhtml',1,'(Global Namespace)']]],
+  ['readme_0',['README',['../md_doc_decisions_README.xhtml',1,'(Global Namespace)'],['../md_hardware_README.xhtml',1,'(Global Namespace)']]],
+  ['readme_2emd_1',['README.md',['../doc_2decisions_2README_8md.xhtml',1,'(Global Namespace)'],['../hardware_2README_8md.xhtml',1,'(Global Namespace)'],['../README_8md.xhtml',1,'(Global Namespace)']]],
   ['refresh_5fdisplay_2',['refresh_display',['../display_8cpp.xhtml#a6fd55a287e038568d2304927ec8b2449',1,'refresh_display():&#160;display.cpp'],['../display_8h.xhtml#a6fd55a287e038568d2304927ec8b2449',1,'refresh_display():&#160;display.cpp']]]
 ];
--- develop/search/files_7.js
+++ develop/search/files_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_2emd_0',['README.md',['../doc_2design-decisions_2README_8md.xhtml',1,'(Global Namespace)'],['../hardware_2README_8md.xhtml',1,'(Global Namespace)'],['../README_8md.xhtml',1,'(Global Namespace)']]]
+  ['readme_2emd_0',['README.md',['../doc_2decisions_2README_8md.xhtml',1,'(Global Namespace)'],['../hardware_2README_8md.xhtml',1,'(Global Namespace)'],['../README_8md.xhtml',1,'(Global Namespace)']]]
 ];
