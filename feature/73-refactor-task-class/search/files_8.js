4194ad6696057196982c4a460b4cc7ecfeb07ff9
dhebbeker <dhebbeker@users.noreply.github.com>
2023-11-04 14:31:20 +0000

    deploy: 21380235de4e96f5126e602cff7e46bcee0a0528

---
+++ feature/73-refactor-task-class/search/files_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['software_2darchitecture_2emd_0',['software-architecture.md',['../software-architecture_8md.xhtml',1,'']]]
+];
---
+++ feature/73-refactor-task-class/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['refresh_5fdisplay_0',['refresh_display',['../display_8cpp.xhtml#a6fd55a287e038568d2304927ec8b2449',1,'refresh_display():&#160;display.cpp'],['../display_8h.xhtml#a6fd55a287e038568d2304927ec8b2449',1,'refresh_display():&#160;display.cpp']]]
+];
