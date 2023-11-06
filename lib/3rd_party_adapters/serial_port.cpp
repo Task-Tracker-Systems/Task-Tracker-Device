86367576513b0eeb351e7fd355e20a6f2fc1971e
David Hebbeker <david@hebbeker.info>
2023-11-06 08:16:08 +0100

    Add serial port object for stream input.

--- lib/3rd_party_adapters/serial_port.cpp
+++ lib/3rd_party_adapters/serial_port.cpp
@@ -1,15 +1,26 @@
 #include "serial_port.hpp"
+#include "SerialInputStreamBuffer.hpp"
 #include "SerialOutputStreamBuffer.hpp"
 #include <Arduino.h>
+#include <cstddef>
 #include <iterator>
 
 static SerialOutputStreamBuffer::char_type serial_output_buffer[255];
 static SerialOutputStreamBuffer serialOutputStreamBuffer(std::begin(serial_output_buffer), std::end(serial_output_buffer));
 static std::ostream serialOutputStream(&serialOutputStreamBuffer);
 
+/**
+ * According to the Arduino reference, the input buffer has a size of 64 bytes.
+ */
+static constexpr std::size_t sizeOfInputBuffer = 64;
+static SerialInputStreamBuffer::char_type serial_input_buffer[sizeOfInputBuffer];
+static SerialInputStreamBuffer serialInputStreamBuffer(std::begin(serial_input_buffer), std::end(serial_input_buffer));
+static std::istream serialInputStream(&serialInputStreamBuffer);
+
 namespace serial_port
 {
 std::ostream &cout = serialOutputStream;
+std::istream &cin = serialInputStream;
 
 void initialize()
 {
