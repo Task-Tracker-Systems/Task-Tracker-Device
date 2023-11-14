4f6a95ad62640c2c3232b32bcdaeb0c79f621182
dhebbeker <dhebbeker@users.noreply.github.com>
2023-11-14 09:08:59 +0000

    deploy: 302f89a6cb87d2cb9515f7a824c05e9ffad15e5a

---
+++ feature/add_adapter_to_read_lines_via_arduino_serial/search/functions_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['getid_0',['getId',['../classTask.xhtml#ae9793dbc18f60c07e324ff6150bc9cf2',1,'Task']]],
+  ['getlabel_1',['getLabel',['../classTask.xhtml#ada97787ea9e1d67d6708eb6c306629dc',1,'Task']]],
+  ['getline_2',['getLine',['../namespaceserial__port.xhtml#ab6c8af59bd81882c7b16c860ce5fdf52',1,'serial_port']]],
+  ['getrecordedduration_3',['getRecordedDuration',['../classTask.xhtml#a35c5ec0d50d2a92028a008721d132d5d',1,'Task']]]
+];
