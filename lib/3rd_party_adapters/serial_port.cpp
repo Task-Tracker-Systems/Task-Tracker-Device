aaa607a7be7aa6b85477426f763fba10137e23f9
David Hebbeker <david@hebbeker.info>
2023-11-14 19:41:15 +0100

    Statically assert character type of serial_port is compatible to the one of Arduino Serial.

--- lib/3rd_party_adapters/serial_port.cpp
+++ lib/3rd_party_adapters/serial_port.cpp
@@ -2,6 +2,7 @@
 #include "SerialOutputStreamBuffer.hpp"
 #include <Arduino.h>
 #include <iterator>
+#include <type_traits>
 
 static SerialOutputStreamBuffer::char_type serial_output_buffer[255];
 static SerialOutputStreamBuffer serialOutputStreamBuffer(std::begin(serial_output_buffer), std::end(serial_output_buffer));
@@ -9,6 +10,8 @@ static std::basic_ostream<SerialOutputStreamBuffer::char_type> serialOutputStrea
 
 namespace serial_port
 {
+static_assert(std::is_same_v<CharType, std::remove_cv_t<std::remove_reference_t<decltype(*Serial.readString().c_str())>>>);
+
 static StringHandler incomingStringHandler;
 
 std::basic_ostream<CharType> &cout = serialOutputStream;
