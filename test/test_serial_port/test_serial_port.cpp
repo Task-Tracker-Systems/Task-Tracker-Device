2b62743974687e2baacf82ae05995aadacab717d
David Hebbeker <david@hebbeker.info>
2023-11-14 10:07:07 +0100

    Add test case for serial_port::getLine()

--- test/test_serial_port/test_serial_port.cpp
+++ test/test_serial_port/test_serial_port.cpp
@@ -1,5 +1,6 @@
 #include "serial_port.hpp"
 #include <ArduinoFake.h>
+#include <cstring>
 #include <unity.h>
 
 using namespace fakeit;
@@ -21,11 +22,31 @@ void test_initialize()
     Verify(OverloadedMethod(ArduinoFake(Serial), begin, void(unsigned long))).AtLeastOnce();
 }
 
+void test_getLine()
+{
+    {
+        // Test empty
+        When(Method(ArduinoFake(Serial), available)).Return(0);
+        const auto result = serial_port::getLine();
+        TEST_ASSERT_FALSE(result.has_value());
+    }
+
+    {
+        // Test non-empty
+        constexpr auto testString = "Hello World";
+        When(Method(ArduinoFake(Serial), available)).Return(std::strlen(testString) + 1);
+        When(Method(ArduinoFake(Serial), readStringUntil)).Return(testString);
+        const auto result = serial_port::getLine();
+        TEST_ASSERT_EQUAL_STRING(testString, result.value_or("error").c_str());
+    }
+}
+
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_initialize);
+    RUN_TEST(test_getLine);
 
     UNITY_END();
 }
