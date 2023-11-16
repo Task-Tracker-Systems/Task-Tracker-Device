e4e5219b06da6f01e3b3c8bc5b154e176304d792
David Hebbeker <david@hebbeker.info>
2023-11-16 11:49:53 +0100

    `trim()`: Implement trimming front.

--- lib/application_business_rules/trim.hpp
+++ lib/application_business_rules/trim.hpp
@@ -1,8 +1,20 @@
 #pragma once
+#include <iterator>
 #include <locale>
+
+template <typename SequenceT>
+SequenceT &ltrim(SequenceT &input, const std::locale &locale = std::locale())
+{
+    const auto begin_trimmed = std::find_if(
+        std::begin(input),
+        std::end(input),
+        [&](const auto ch) { return !std::isspace(ch, locale); });
+    input = SequenceT(begin_trimmed, std::end(input));
+    return input;
+}
 
 template <typename SequenceT>
 SequenceT &trim(SequenceT &input, const std::locale &locale = std::locale())
 {
-    return input;
+    return ltrim(input);
 }
