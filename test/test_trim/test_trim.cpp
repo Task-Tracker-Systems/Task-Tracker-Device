44a78351ea8c76030569fc044bd621a84276de0e
David Hebbeker <david@hebbeker.info>
2023-11-16 10:51:54 +0100

    Add skeleton for trim function.

---
+++ test/test_trim/test_trim.cpp
@@ -0,0 +1,22 @@
+#include <fakeit.hpp>
+#include <trim.hpp>
+#include <unity.h>
+
+using namespace fakeit;
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST();
+
+    UNITY_END();
+}
