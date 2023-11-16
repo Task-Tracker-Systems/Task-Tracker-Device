40860c2750566934402a95cbe8041ee63d23c721
David Hebbeker <david@hebbeker.info>
2023-11-16 17:17:41 +0100

    Extend trim such that it also accepts rvalues as arguments.

--- lib/application_business_rules/trim.hpp
+++ lib/application_business_rules/trim.hpp
@@ -1,20 +1,27 @@
 #pragma once
 #include <iterator>
 #include <locale>
+#include <utility>
 
 template <typename SequenceT>
-SequenceT &ltrim(SequenceT &input, const std::locale &locale = std::locale())
+SequenceT ltrim(SequenceT &&input, const std::locale &locale = std::locale())
 {
     const auto begin_trimmed = std::find_if(
         std::begin(input),
         std::end(input),
         [&](const auto ch) { return !std::isspace(ch, locale); });
-    input = SequenceT(begin_trimmed, std::end(input));
+    return SequenceT(begin_trimmed, std::end(input));
+}
+
+template <typename SequenceT>
+SequenceT &ltrim(SequenceT &input, const std::locale &locale = std::locale())
+{
+    input = ltrim(std::move(input));
     return input;
 }
 
 template <typename SequenceT>
-SequenceT &rtrim(SequenceT &input, const std::locale &locale = std::locale())
+SequenceT rtrim(SequenceT &&input, const std::locale &locale = std::locale())
 {
     const auto end_trimmed = std::find_if(
         std::rbegin(input),
@@ -25,7 +32,14 @@ SequenceT &rtrim(SequenceT &input, const std::locale &locale = std::locale())
 }
 
 template <typename SequenceT>
-SequenceT &trim(SequenceT &input, const std::locale &locale = std::locale())
+SequenceT &rtrim(SequenceT &input, const std::locale &locale = std::locale())
+{
+    input = rtrim(std::move(input));
+    return input;
+}
+
+template <typename SequenceT>
+SequenceT &trim(SequenceT &&input, const std::locale &locale = std::locale())
 {
     return rtrim(ltrim(input));
 }
