9d5b0af71847c6a62050f976cf9da0ffbdd228ab
David Hebbeker <david@hebbeker.info>
2023-11-16 11:03:45 +0100

    Add test case for trim.

--- test/test_trim/test_trim.cpp
+++ test/test_trim/test_trim.cpp
@@ -1,4 +1,5 @@
 #include <fakeit.hpp>
+#include <string>
 #include <trim.hpp>
 #include <unity.h>
 
@@ -12,11 +13,21 @@ void tearDown()
 {
 }
 
+void test_nothingToTrim()
+{
+    std::string stringFine = "Hello World";
+    const std::string stringExpected = stringFine;
+
+    trim(stringFine);
+
+    TEST_ASSERT_EQUAL_STRING(stringExpected.c_str(), stringFine.c_str());
+}
+
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
 
-    RUN_TEST();
+    RUN_TEST(test_nothingToTrim);
 
     UNITY_END();
 }
