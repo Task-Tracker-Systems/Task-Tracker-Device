90f58dc623d61707412b1e2b18597d719834e72c
dhebbeker <dhebbeker@users.noreply.github.com>
2023-11-25 09:45:58 +0000

    deploy: 1f162afeade766b655ea55c62c979402093e6499

---
+++ feature/add-trim-functions/search/all_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main_0',['main',['../namespacemain.xhtml',1,'main'],['../test__serial__port_8cpp.xhtml#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;test_serial_port.cpp'],['../task_8cpp.xhtml#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;task.cpp'],['../test__trim_8cpp.xhtml#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;test_trim.cpp']]],
+  ['main_2ecpp_1',['main.cpp',['../main_8cpp.xhtml',1,'']]],
+  ['main_2ehpp_2',['main.hpp',['../main_8hpp.xhtml',1,'']]]
+];
