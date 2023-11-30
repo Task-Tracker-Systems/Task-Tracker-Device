764427c88e0d275a9914788a05af011747d5d9de
David Hebbeker <david@hebbeker.info>
2023-11-30 16:10:00 +0100

    Add documentation to string helpers.

--- lib/enterprise_business_rules/string_helpers.hpp
+++ lib/enterprise_business_rules/string_helpers.hpp
@@ -7,10 +7,37 @@
 #include <string>
 #include <vector>
 
+/**
+ * Compares two null-terminated byte strings lexicographically.
+ * 
+ * This is a wrapper to std::wcscmp() and std::strcmp() specialized for 
+ * the character types.
+ * 
+ * \tparam CharType is the type of character to be used
+ * \param lhs pointer to the null-terminated byte strings to compare
+ * \param rhs pointer to the null-terminated byte strings to compare
+ * \retval Negative value if lhs appears before rhs in lexicographical order.
+ * \retval Zero if lhs and rhs compare equal.
+ * \retval Positive value if lhs appears after rhs in lexicographical order.
+ */
 template <typename CharType>
 int strcmp_g(const CharType *const lhs, const CharType *const rhs);
 
 // Tokenize a string using std::quoted
+/**
+ * Tokenize a string while considering quoted sequences.
+ * 
+ * The input is analyzed and for each sub-sequence which is separated by whitespaces, a token is extracted.
+ * With exception of sub-sequences which are quoted.
+ * Those are extracted as a whole.
+ * 
+ * Quotes are according to `std::quoted`: '"' and escape characters `\`.
+ * 
+ * \tparam CharType is the type of character to be used (for example `char`)
+ * \param input is the string to be analyzed
+ * \throws std::runtime_error in case the extraction of a sub-sequence failed
+ * \returns a set of strings
+ */
 template <typename CharType>
 std::vector<std::basic_string<CharType>> tokenizeQuoted(const std::basic_string<CharType> &input)
 {
