dbcba0f547e8850202fd62bc254dbd82682e8ac1
David Hebbeker <david@hebbeker.info>
2023-12-10 12:22:21 +0100

    Write `inputIsr()`.

--- lib/board_adapters/board.cpp
+++ lib/board_adapters/board.cpp
@@ -1,13 +1,47 @@
 #include "board.hpp"
 #include "board_pins.hpp"
 #include <Arduino.h>
+#include <iterator>
+#include <stdexcept>
+#include <utility>
+
+static board::HmiHandler hmiHandler;
 
 void board::setHmiHandler(const HmiHandler callbackFunction)
 {
+    hmiHandler = callbackFunction;
 }
 
 static void inputIsr()
 {
+    if (!hmiHandler)
+    {
+        return;
+    }
+    /* loop through inputs, evaluate and call handler if appropriate */
+    constexpr std::pair<PinType, board::HmiSelection> selectionForPins[] = {
+        {board::button::pin::task1, board::HmiSelection::TASK1},
+        {board::button::pin::task2, board::HmiSelection::TASK2},
+        {board::button::pin::task3, board::HmiSelection::TASK3},
+        {board::button::pin::task4, board::HmiSelection::TASK4},
+        {board::button::pin::up, board::HmiSelection::UP},
+        {board::button::pin::down, board::HmiSelection::DOWN},
+        {board::button::pin::enter, board::HmiSelection::ENTER},
+        {board::button::pin::back, board::HmiSelection::BACK},
+    };
+    static bool pinsAlreadyActive[std::size(selectionForPins)] = {false};
+    auto it_pinAlreadyActive = std::begin(pinsAlreadyActive);
+    for (const auto &selectionForPin : selectionForPins)
+    {
+        constexpr auto activeState = LOW; // buttons are active low
+        const bool isActiveNow = digitalRead(selectionForPin.first) == activeState;
+        if (!(*it_pinAlreadyActive) && isActiveNow) // react only on activation
+        {
+            (*hmiHandler)(selectionForPin.second);
+        }
+        *it_pinAlreadyActive = isActiveNow;
+        it_pinAlreadyActive++;
+    }
 }
 
 void board::setup()
@@ -32,7 +66,7 @@ void board::setup()
     for (const auto inputPin : inputPins)
     {
         pinMode(inputPin, INPUT_PULLUP);
-        attachInterrupt(digitalPinToInterrupt(inputPin), &inputIsr, FALLING);
+        attachInterrupt(digitalPinToInterrupt(inputPin), &inputIsr, CHANGE);
     }
     for (const auto outputPin : outputPins)
     {
@@ -52,8 +86,17 @@ static PinType mapIndicatorIdToPin(const board::StatusIndicatorId id)
     case board::StatusIndicatorId::TASK1:
         return board::led::pin::task1;
         break;
-    default:
+    case board::StatusIndicatorId::TASK2:
+        return board::led::pin::task2;
         break;
+    case board::StatusIndicatorId::TASK3:
+        return board::led::pin::task3;
+        break;
+    case board::StatusIndicatorId::TASK4:
+        return board::led::pin::task4;
+        break;
+    default:
+        throw std::runtime_error();
     }
 }
 
