6da634f91248cf3e20326e79a13ba4162400d3c6
David Hebbeker <david@hebbeker.info>
2023-12-10 14:24:56 +0100

    Remove outsourced functionality from main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 /**
  * \file
  *
- * - get event
- *   - read pins
- *   - identify/generate event
  * - setup
  *   - configure hardware (pins)
  *   - initialize
@@ -21,51 +18,14 @@
 #include "display.h"
 #include "pitches.hpp"
 #include <Arduino.h>
+#include <HmiCoordinator.hpp>
 #include <cstdint>
 #include <serial_port_interface.hpp>
-
-/**
- * Events are the beginning of a pressed button.
- * 
- * @returns 1-8 or 0 in case of no event
- */
-static std::uint8_t getEvent()
-{
-    constexpr PinType inputPins[] = {
-        board::button::pin::task1,
-        board::button::pin::task2,
-        board::button::pin::task3,
-        board::button::pin::task4,
-        board::button::pin::up,
-        board::button::pin::down,
-        board::button::pin::enter,
-        board::button::pin::back,
-    };
-    static PinType oldValue = 0U;
-    PinType newValue = 0U;
-    std::uint8_t result = 0U;
-
-    std::uint8_t candidateEvent = 1;
-    for (const auto pin : inputPins)
-    {
-        if (digitalRead(pin) == LOW) // buttons are active low
-        {
-            newValue = candidateEvent;
-        }
-        candidateEvent++;
-    }
-
-    if (newValue != 0 && oldValue == 0)
-    {
-        result = newValue;
-    }
-    oldValue = newValue;
-    return result;
-}
 
 void setup()
 {
     serial_port::initialize();
+    hmi_coordinator::setup();
     setup_display();
     static constexpr const auto programIdentificationString = __FILE__ " compiled at " __DATE__ " " __TIME__;
     serial_port::cout << std::endl
@@ -74,33 +34,5 @@ void setup()
 void loop()
 {
     constexpr unsigned long loopDurationMs = 250;
-    const auto event = getEvent();
-    constexpr PinType outputPins[] = {
-        board::led::pin::task1,
-        board::led::pin::task2,
-        board::led::pin::task3,
-        board::led::pin::task4,
-    };
-    if (event)
-    {
-        Serial.printf("Process event '%u'.\n", event);
-        constexpr std::uint16_t notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
-        tone(board::buzzer::pin::on_off, notes[event - 1], loopDurationMs);
-        if (event < sizeof(outputPins) / sizeof(outputPins[0]) + 1)
-        {
-            constexpr int minBrightness = 0;
-            analogWrite(outputPins[event - 1], minBrightness);
-        }
-    }
-    else
-    {
-        for (const auto pin : outputPins)
-        {
-            constexpr int maxBrightness = 255;
-            constexpr int brightness = maxBrightness * 25 / 100.0;
-            analogWrite(pin, brightness);
-        }
-    }
-    delay(loopDurationMs);
     refresh_display(); // Animate bitmaps
 }
