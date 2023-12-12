6850acd07aac4952ac9d17374843379111e6422c
David Hebbeker <david@hebbeker.info>
2023-12-12 08:29:57 +0100

    Fix some compiler errors.

--- lib/application_business_rules/HmiCoordinator.cpp
+++ lib/application_business_rules/HmiCoordinator.cpp
@@ -7,16 +7,22 @@
 #include <pitches.hpp>
 #include <stdexcept>
 
+template <class Enum>
+static constexpr auto enumToInteger(const Enum enumerator)
+{
+    return static_cast<std::underlying_type_t<Enum>>(enumerator);
+}
+
 static Task &mapTaskSelectionToTask(const board::HmiSelection taskSelection)
 {
-    const std::size_t taskIndex = taskSelection - board::HmiSelection::TASK1;
-    return *std::next(std::begin(device::tasks), taskIndex);
+    const std::size_t taskIndex = enumToInteger(taskSelection) - enumToInteger(board::HmiSelection::TASK1);
+    return std::next(std::begin(device::tasks), taskIndex)->second;
 }
 
 static unsigned int mapSelectionToFrequency(const board::HmiSelection selection)
 {
-    constexpr auto notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
-    return notes[selection];
+    constexpr decltype(note::c3) notes[] = {note::c3, note::d3, note::e3, note::f3, note::g3, note::a3, note::b3, note::c4};
+    return notes[enumToInteger(selection)];
 }
 
 static board::StatusIndicatorId mapTaskToStatusIndicator(const board::HmiSelection selection)
@@ -36,14 +42,14 @@ static board::StatusIndicatorId mapTaskToStatusIndicator(const board::HmiSelecti
         return board::StatusIndicatorId::TASK4;
         break;
     default:
-        throw std::runtime_error();
+        throw std::runtime_error("no status indicator for this task available");
         break;
     }
 }
 
 static void handleHmiSelection(const board::HmiSelection selection)
 {
-    serial_port::cout << "Handle event nr. " << selection << std::endl;
+    serial_port::cout << "Handle event nr. " << enumToInteger(selection) << std::endl;
     using namespace std::chrono_literals;
     board::playTone(mapSelectionToFrequency(selection), 1s);
     switch (selection)
@@ -51,7 +57,7 @@ static void handleHmiSelection(const board::HmiSelection selection)
     case board::HmiSelection::TASK1:
     case board::HmiSelection::TASK2:
     case board::HmiSelection::TASK3:
-    case board::HmiSelection::TASK4:
+    case board::HmiSelection::TASK4: {
         auto &task = mapTaskSelectionToTask(selection);
         if (task.isRunning())
         {
@@ -63,6 +69,7 @@ static void handleHmiSelection(const board::HmiSelection selection)
         }
         board::setStatusIndicator(mapTaskToStatusIndicator(selection), task.isRunning());
         break;
+    }
     default:
         break;
     }
