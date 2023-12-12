c4e0abdf8493b7dbebd396aeb676f6dad4a510b4
David Hebbeker <david@hebbeker.info>
2023-12-12 08:52:04 +0100

    WIP: Attempt to fix incompatability std::function ↔ function pointer.

--- lib/board_adapters/board.cpp
+++ lib/board_adapters/board.cpp
@@ -6,18 +6,58 @@
 #include <stdexcept>
 #include <utility>
 
+static board::HmiHandler callBack;
+
+template <board::HmiSelection SELECTION>
+static void isr()
+{
+    callBack(SELECTION);
+}
+
+template <class T, std::size_t N>
+struct H
+{
+    static void constexpr configureOutput(const T &selectionForPin)
+    {
+        pinMode(selectionForPin.first, INPUT_PULLUP);
+        attachInterrupt(
+            digitalPinToInterrupt(selectionForPin.first),
+            (isr<selectionForPin.second>),
+            FALLING);
+    }
+
+    template <T (&A)[N]>
+    constexpr static void aH1()
+    {
+        aH2<A>(std::make_index_sequence<N>());
+    }
+
+    template <T (&A)[N], std::size_t... Is>
+    constexpr static void aH2(const std::index_sequence<Is...>)
+    {
+        ((configureOutput(A[Is])), ...);
+    }
+};
+
+template <class T, std::size_t N>
+static H<T, N> makeH(T (&)[N])
+{
+    return H<T, N>();
+}
+
+static constexpr std::pair<PinType, board::HmiSelection> selectionForPins[] = {
+    {board::button::pin::task1, board::HmiSelection::TASK1},
+    {board::button::pin::task2, board::HmiSelection::TASK2},
+    {board::button::pin::task3, board::HmiSelection::TASK3},
+    {board::button::pin::task4, board::HmiSelection::TASK4},
+    {board::button::pin::up, board::HmiSelection::UP},
+    {board::button::pin::down, board::HmiSelection::DOWN},
+    {board::button::pin::enter, board::HmiSelection::ENTER},
+    {board::button::pin::back, board::HmiSelection::BACK},
+};
+
 void board::setup(const HmiHandler callbackFunction)
 {
-    constexpr std::pair<PinType, board::HmiSelection> selectionForPins[] = {
-        {board::button::pin::task1, board::HmiSelection::TASK1},
-        {board::button::pin::task2, board::HmiSelection::TASK2},
-        {board::button::pin::task3, board::HmiSelection::TASK3},
-        {board::button::pin::task4, board::HmiSelection::TASK4},
-        {board::button::pin::up, board::HmiSelection::UP},
-        {board::button::pin::down, board::HmiSelection::DOWN},
-        {board::button::pin::enter, board::HmiSelection::ENTER},
-        {board::button::pin::back, board::HmiSelection::BACK},
-    };
     constexpr PinType outputPins[] = {
         board::led::pin::task1,
         board::led::pin::task2,
@@ -25,13 +65,7 @@ void board::setup(const HmiHandler callbackFunction)
         board::led::pin::task4,
         board::buzzer::pin::on_off,
     };
-    for (const auto &selectionForPin : selectionForPins)
-    {
-        pinMode(selectionForPin.first, INPUT_PULLUP);
-        attachInterrupt(digitalPinToInterrupt(selectionForPin.first),
-                        std::bind(callbackFunction, selectionForPin.second),
-                        FALLING);
-    }
+    makeH(selectionForPins).aH1<selectionForPins>();
     for (const auto outputPin : outputPins)
     {
         pinMode(outputPin, OUTPUT);
@@ -60,7 +94,7 @@ static PinType mapIndicatorIdToPin(const board::StatusIndicatorId id)
         return board::led::pin::task4;
         break;
     default:
-        throw std::runtime_error();
+        throw std::runtime_error("no pin defined for selected status indicator");
         break;
     }
 }
