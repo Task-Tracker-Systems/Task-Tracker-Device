c8cb07fe01707dda1319cc233ee0ba8ea1590707
dhebbeker <dhebbeker@users.noreply.github.com>
2023-12-17 21:20:05 +0000

    deploy: 862bce4c6667e17bd288367417fdbf6f8fbb73db

--- feature/clean-structure-for-hmi-interaction/search/functions_8.js
+++ feature/clean-structure-for-hmi-interaction/search/functions_8.js
@@ -3,5 +3,5 @@ var searchData=
   ['label_0',['label',['../task_8cpp.xhtml#ae053df6e863615ebd54791414f975e92',1,'task.cpp']]],
   ['ledstatusindicator_1',['LedStatusIndicator',['../classLedStatusIndicator.xhtml#a5fe6a24b3ad0ea6a5cb5d5de5a469afc',1,'LedStatusIndicator']]],
   ['linehandler_2',['lineHandler',['../test__serial__port_8cpp.xhtml#ae130579175b5cb1f6359a4306c2b241a',1,'test_serial_port.cpp']]],
-  ['loop_3',['loop',['../classPresenter.xhtml#a08de6e9eb7f340e4bd532dc50c29b815',1,'Presenter::loop()'],['../classProcessHmiInputs.xhtml#a35691fc7255d8ad9e423a59f4bde9831',1,'ProcessHmiInputs::loop()'],['../classMenu.xhtml#a23bb40aa976a2d694d5fcce7180fc53c',1,'Menu::loop()'],['../namespacemain.xhtml#aa018644f5f4d7111540bb4777634ec42',1,'main::loop()'],['../time-tracker_8ino.xhtml#afe461d27b9c48d5921c00d521181f12f',1,'loop():&#160;time-tracker.ino']]]
+  ['loop_3',['loop',['../classMenu.xhtml#a23bb40aa976a2d694d5fcce7180fc53c',1,'Menu::loop()'],['../classPresenter.xhtml#a08de6e9eb7f340e4bd532dc50c29b815',1,'Presenter::loop()'],['../classProcessHmiInputs.xhtml#a35691fc7255d8ad9e423a59f4bde9831',1,'ProcessHmiInputs::loop()'],['../namespacemain.xhtml#aa018644f5f4d7111540bb4777634ec42',1,'main::loop()'],['../time-tracker_8ino.xhtml#afe461d27b9c48d5921c00d521181f12f',1,'loop():&#160;time-tracker.ino']]]
 ];
