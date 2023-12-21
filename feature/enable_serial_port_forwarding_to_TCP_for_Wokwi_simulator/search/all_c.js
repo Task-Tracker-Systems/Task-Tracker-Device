81c0a000772e0048a63cfe0dac7d552e5da7df20
dhebbeker <dhebbeker@users.noreply.github.com>
2023-12-21 13:43:55 +0000

    deploy: 24df6f7b689a2b3fb28de42225883ff25dbc7900

---
+++ feature/enable_serial_port_forwarding_to_TCP_for_Wokwi_simulator/search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['n_0',['n',['../test__cli_8cpp.xhtml#a76f11d9a0a47b94f72c2d0e77fb32240',1,'test_cli.cpp']]],
+  ['note_1',['note',['../namespacenote.xhtml',1,'']]]
+];
---
+++ feature/enable_serial_port_forwarding_to_TCP_for_Wokwi_simulator/search/all_e.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['pintype_0',['PinType',['../framework__config_8hpp.xhtml#a8f084eabb88c2ccfb8dd92c6361befe6',1,'framework_config.hpp']]],
+  ['pitches_2ehpp_1',['pitches.hpp',['../pitches_8hpp.xhtml',1,'']]],
+  ['programidentificationstring_2',['programIdentificationString',['../time-tracker_8ino.xhtml#a2b95c44e365489a256863888655ac8f8',1,'time-tracker.ino']]],
+  ['protocol_2ecpp_3',['Protocol.cpp',['../Protocol_8cpp.xhtml',1,'']]],
+  ['protocol_2ehpp_4',['Protocol.hpp',['../Protocol_8hpp.xhtml',1,'']]],
+  ['protocolhandler_5',['ProtocolHandler',['../classProtocolHandler.xhtml',1,'']]]
+];
