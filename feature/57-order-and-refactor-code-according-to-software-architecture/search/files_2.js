bd4862c9761fd744946762a107841d9b7e93349c
dhebbeker <dhebbeker@users.noreply.github.com>
2024-01-07 16:12:27 +0000

    deploy: a7cc797026392af4e21b0ea6f471609b15c00350

--- feature/57-order-and-refactor-code-according-to-software-architecture/search/files_2.js
+++ feature/57-order-and-refactor-code-according-to-software-architecture/search/files_2.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['display_2ecpp_0',['display.cpp',['../display_8cpp.xhtml',1,'']]],
-  ['display_2ehpp_1',['display.hpp',['../display_8hpp.xhtml',1,'']]],
-  ['display_5finterface_2ehpp_2',['display_interface.hpp',['../display__interface_8hpp.xhtml',1,'']]],
-  ['dr_2d001_2emd_3',['dr-001.md',['../dr-001_8md.xhtml',1,'']]]
+  ['display_5finterface_2ehpp_1',['display_interface.hpp',['../display__interface_8hpp.xhtml',1,'']]],
+  ['dr_2d001_2emd_2',['dr-001.md',['../dr-001_8md.xhtml',1,'']]]
 ];
