5e3a7d006698c235cf4ce8d3747e47c6b52ad5f4
dhebbeker <dhebbeker@users.noreply.github.com>
2024-01-07 16:25:35 +0000

    deploy: 8df1f78c0f7722124be02427b8cafff0be10b463

--- feature/57-order-and-refactor-code-according-to-software-architecture/search/all_12.js
+++ feature/57-order-and-refactor-code-according-to-software-architecture/search/all_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['up_0',['UP',['../namespaceboard.xhtml#a7ea5fbeae4f85d4a1eaf8d601497816eafbaedde498cdead4f2780217646e9ba1',1,'board']]],
-  ['up_1',['up',['../namespaceboard_1_1button_1_1pin.xhtml#ad6d1c567741414da0d6b0ecdae4d425d',1,'board::button::pin']]]
+  ['up_1',['up',['../namespaceboard_1_1button_1_1pin.xhtml#ad6d1c567741414da0d6b0ecdae4d425d',1,'board::button::pin']]],
+  ['utilities_2',['Utilities',['../utilities.xhtml',1,'']]]
 ];
