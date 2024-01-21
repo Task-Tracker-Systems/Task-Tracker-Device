0ee6f492dbe33602b484e0b04a753d6ef05b67b8
David Hebbeker <david@hebbeker.info>
2024-01-21 16:26:30 +0100

    WIP: Write test for ProcessHmiInputs.

--- test/test_hmi/test_hmi.cpp
+++ test/test_hmi/test_hmi.cpp
@@ -1,4 +1,8 @@
 #include <ArduinoFake.h>
+#include <cstdint>
+#include <iomanip>
+#include <ios>
+#include <iostream>
 #include <serial_interface/serial_port_interface.hpp>
 #include <unity.h>
 #include <user_interaction/IKeypad.hpp>
@@ -6,7 +10,10 @@
 #include <user_interaction/Presenter.hpp>
 #include <user_interaction/ProcessHmiInputs.hpp>
 #include <user_interaction/display_factory_interface.hpp>
+#include <user_interaction/keypad_factory_interface.hpp>
 #include <user_interaction/statusindicators_factory_interface.hpp>
+#include <utility>
+#include <vector>
 
 using namespace fakeit;
 
@@ -36,10 +43,18 @@ void tearDown()
 
 void test_Controller()
 {
-    Mock<IKeypad> fakeKeypad;
+    std::vector<std::pair<std::uint8_t, void (*)(void)>> isr_collection;
+
+    Fake(Method(ArduinoFake(), pinMode));
+    When(Method(ArduinoFake(), attachInterrupt)).AlwaysDo([&isr_collection](const auto interruptNum, const auto userFunc, const auto mode) {
+        isr_collection.emplace_back(interruptNum, userFunc);
+        std::cout << "Added ISR for interrupt number " << static_cast<unsigned int>(interruptNum)
+                  << " function pointer " << std::showbase << std::hex << reinterpret_cast<std::intptr_t>(userFunc) << std::resetiosflags(std::ios::showbase | std::ios::basefield) << std::endl;
+    });
+
     static Menu singleMenu(board::getDisplay());
     static Presenter presenter(singleMenu, board::getStatusIndicators());
-    ProcessHmiInputs processor(presenter, fakeKeypad.get());
+    ProcessHmiInputs processor(presenter, board::getKeypad());
     /*
     {
         // First no key is pressed
