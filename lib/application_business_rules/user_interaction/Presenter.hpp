e4c77d0368fba226599d8854955819abf18caf3c
David Hebbeker <david@hebbeker.info>
2024-01-27 16:39:03 +0100

    Add documentation to Presenter and Processor.

--- lib/application_business_rules/user_interaction/Presenter.hpp
+++ lib/application_business_rules/user_interaction/Presenter.hpp
@@ -12,6 +12,17 @@ enum class TaskIndicatorState
 
 using TaskIndex = std::size_t;
 
+/**
+ * Responsible for advertising information and alerts to a human user.
+ *
+ * This class decides what technical means shall be used to provide logical information.
+ * For example if a status is shown using a display or an LED.
+ * Also it may use other than visual means to signal events.
+ * For example sound or vibration.
+ *
+ * Designated users of this class are use case interactors or other forms of use case implementations.
+ * This class may depend on devices or device interfaces which in turn will be realized by hardware (or board assembly).
+ */
 class Presenter
 {
   public:
--- lib/application_business_rules/user_interaction/ProcessHmiInputs.hpp
+++ lib/application_business_rules/user_interaction/ProcessHmiInputs.hpp
@@ -4,6 +4,14 @@
 class Presenter;
 class IKeypad;
 
+/**
+ * Responsible for handling the inputs from human interface devices.
+ *
+ * Inputs from human interface devices will be processed using the application logic.
+ * The \ref Presenter is used to feedback information back to the human user.
+ *
+ * \dotfile presenter_collaboration.dot "information flow using the Presenter"
+ */
 class ProcessHmiInputs
 {
   public:
