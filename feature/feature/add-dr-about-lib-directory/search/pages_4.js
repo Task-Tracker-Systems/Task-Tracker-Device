e2330ca4468fcfabb9e2e8932739dc2d130b505b
dhebbeker <dhebbeker@users.noreply.github.com>
2024-01-28 21:13:04 +0000

    deploy: 8d4eb0af0e5ea445d2e0e5010b75e206a7b92872

---
+++ feature/feature/add-dr-about-lib-directory/search/pages_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['dr001_20represent_20software_20architecture_20in_20directory_20structure_0',['DR001 Represent Software Architecture in Directory Structure',['../md_doc_decisions_dr_001.xhtml',1,'']]],
+  ['dr002_20root_20directory_20for_20the_20package_27s_20source_20files_1',['DR002 Root Directory for the Package&apos;s source files',['../dr_002.xhtml',1,'']]]
+];
