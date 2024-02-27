238865abb21b7bd79ec5802a373484c001e0792c
David Hebbeker <david@hebbeker.info>
2024-02-27 18:27:51 +0100

    Enhance test.

--- test/test_worker/test_worker.cpp
+++ test/test_worker/test_worker.cpp
@@ -1,5 +1,8 @@
 #include <Worker.hpp>
+#include <chrono>
 #include <unity.h>
+
+using namespace std::chrono_literals;
 
 void setUp(void)
 {
@@ -12,7 +15,10 @@ void tearDown(void)
 void test_run_worker()
 {
     bool flag = false;
-    const auto work = [&flag]() { flag = true; };
+    const auto work = [&flag]() {
+        std::this_thread::sleep_for(1s);
+        flag = true;
+    };
     Worker w(work);
     w.wait_until_finished();
     TEST_ASSERT_TRUE(flag);
@@ -21,5 +27,6 @@ void test_run_worker()
 int main()
 {
     UNITY_BEGIN();
+    RUN_TEST(test_run_worker);
     return UNITY_END();
 }
