76d696a0a6613227a64a71b594701ff185ceca1b
David Hebbeker <david@hebbeker.info>
2024-03-10 20:08:28 +0100

    Add more tests.

--- test/test_worker/test_worker.cpp
+++ test/test_worker/test_worker.cpp
@@ -24,6 +24,18 @@ void test_run_worker()
     TEST_ASSERT_TRUE(flag);
 }
 
+void test_run_worker2()
+{
+    std::atomic<bool> flag = false;
+    const auto work = [&flag]() {
+        flag = true;
+    };
+    std::shared_ptr<Worker> worker;
+    Worker::restart(worker, work, 10ms);
+    std::this_thread::sleep_for(20ms);
+    TEST_ASSERT_TRUE(flag);
+}
+
 void test_abort()
 {
     std::atomic<bool> flag = false;
@@ -33,6 +45,23 @@ void test_abort()
     const auto worker = Worker::spawnNew(work, 100ms);
     std::this_thread::sleep_for(20ms);
     worker->cancelStartup();
+    std::this_thread::sleep_for(100ms);
+    TEST_ASSERT_FALSE(flag);
+}
+
+void test_abort2()
+{
+    std::atomic<bool> flag = false;
+    const auto work = [&flag]() {
+        flag = true;
+    };
+
+    std::shared_ptr<Worker> worker;
+    Worker::restart(worker, work, 100ms);
+    std::this_thread::sleep_for(20ms);
+    Worker::restart(
+        worker, []() {}, 20ms);
+    std::this_thread::sleep_for(140ms);
     TEST_ASSERT_FALSE(flag);
 }
 
@@ -40,6 +69,8 @@ int main()
 {
     UNITY_BEGIN();
     RUN_TEST(test_run_worker);
+    RUN_TEST(test_run_worker2);
     RUN_TEST(test_abort);
+    RUN_TEST(test_abort2);
     return UNITY_END();
 }
