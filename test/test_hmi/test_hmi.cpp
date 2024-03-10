70dae536389c300c5b95a6becfada486c3bde89f
David Hebbeker <david@hebbeker.info>
2024-03-10 13:28:36 +0100

    Adjust test double usage in test.

--- test/test_hmi/test_hmi.cpp
+++ test/test_hmi/test_hmi.cpp
@@ -49,48 +49,54 @@ void tearDown()
 
 void test_Controller()
 {
-    struct IsrHandle
-    {
-        board::PinType associatedPin;
-        void (*isrFunction)(void);
-    };
-
-    std::map<board::PinType, void (*)(void)> isr_collection;
-
+    // irrelevant test doubles
     Fake(Method(ArduinoFake(), pinMode));
     Fake(Method(ArduinoFake(), analogWrite));
     Fake(Method(ArduinoFake(), tone));
-    When(Method(ArduinoFake(), attachInterrupt)).AlwaysDo([&isr_collection](const auto interruptNum, const auto userFunc, const auto mode) {
+
+    // Faking attaching ISRs
+    std::map<int, void (*)(void)> isr_collection;
+    When(Method(ArduinoFake(), attachInterrupt)).AlwaysDo([&isr_collection](const int interruptNum, void (*const userFunc)(void), [[maybe_unused]] const auto mode) {
         isr_collection.emplace(interruptNum, userFunc);
-        std::cout << "Added ISR for interrupt number " << static_cast<unsigned int>(interruptNum)
-                  << " function pointer " << std::showbase << std::hex << reinterpret_cast<std::intptr_t>(userFunc) << std::resetiosflags(std::ios::showbase | std::ios::basefield) << std::endl;
+        std::cout << "Added ISR for interrupt number: " << interruptNum
+                  << " mode: " << mode
+                  << " and function pointer: " << std::showbase << std::hex << reinterpret_cast<std::intptr_t>(userFunc) << std::resetiosflags(std::ios::showbase | std::ios::basefield) << std::endl;
     });
 
+    // get collaborator objects
     Menu singleMenu(board::getDisplay());
     Presenter presenter(singleMenu, board::getStatusIndicators());
     ProcessHmiInputs processor(presenter, board::getKeypad());
+    auto &task1 = std::begin(device::tasks)->second;                      // we are going to test for task 1
+    const auto isrTask1 = isr_collection.find(board::button::pin::task1); // ISR we expect for task 1
+    TEST_ASSERT_NOT_EQUAL(std::end(isr_collection), isrTask1);            // assert we found an ISR for task 1 in the list
 
-    auto &task1 = std::begin(device::tasks)->second;
-    const auto isrTask1 = isr_collection.find(board::button::pin::task1);
-    TEST_ASSERT_NOT_EQUAL(std::end(isr_collection), isrTask1);
-    When(Method(ArduinoFake(), millis)).Return(700); // assume the processor is already running for some time
-    isrTask1->second();                              // start task
+    When(Method(ArduinoFake(), digitalRead).Using(board::button::pin::task1)).Return(LOW); // set task 1 button to low
+    isrTask1->second();                                                                    // trigger interrupt for task 1 button
+
+    // wait for the task to be running
     while (!task1.isRunning())
     {
         // TODO it would be better to explicitly check for the "stop" task to be finished
         std::this_thread::yield(); // give the task handler time to finish before the test interferes
     }
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(1000ms);
-    When(Method(ArduinoFake(), millis)).Return(700 + 1337); // used in ISR for debouncing
-    isrTask1->second();                                     // stop task
+
+    constexpr int millisecondsToWait = 1000;
+    std::this_thread::sleep_for(std::chrono::milliseconds(millisecondsToWait)); // wait a defined time
+
+    When(Method(ArduinoFake(), digitalRead).Using(board::button::pin::task1)).Return(LOW); // set task 1 button to low
+    isrTask1->second();                                                                    // stop task
+
+    // wait for the task to be stopped
     while (task1.isRunning())
     {
         // TODO it would be better to explicitly check for the "start" task to be finished
         std::this_thread::yield(); // give the task handler time to finish before the test interferes
     }
-    const auto measured = std::chrono::duration_cast<std::chrono::milliseconds>(task1.getRecordedDuration());
-    TEST_ASSERT_INT_WITHIN(10, 1000, measured.count());
+
+    // assert results
+    const auto millisecondsMeasured = std::chrono::duration_cast<std::chrono::milliseconds>(task1.getRecordedDuration());
+    TEST_ASSERT_INT_WITHIN(10, millisecondsToWait, millisecondsMeasured.count());
 }
 
 int main(int argc, char **argv)
