3aa20b05c96b855824753d0e5fed0324769b06be
David Hebbeker <david@hebbeker.info>
2025-02-16 16:30:54 +0100

    Add test for FatFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <FFat.h>
 
-// put function declarations here:
-int myFunction(int, int);
+static void testFatFS() {
+  File file = FFat.open("/test.txt", "w");
+  if (!file) {
+    Serial.println("Fehler beim Öffnen der Datei!");
+    return;
+  }
+  file.println("Hallo FatFS!");
+  file.close();
+
+  file = FFat.open("/test.txt", "r");
+  if (file) {
+    Serial.println("Inhalt der Datei:");
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+    Serial.println();
+    file.close();
+  } else {
+    Serial.println("Konnte Datei nicht lesen.");
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  if (!FFat.begin(
+          true)) { // `true` = Formatieren falls kein Dateisystem vorhanden
+    Serial.println("FatFS konnte nicht gestartet werden!");
+    return;
+  }
+
+  Serial.println("FatFS erfolgreich gemountet.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  testFatFS();
+  delay(10000);
 }
