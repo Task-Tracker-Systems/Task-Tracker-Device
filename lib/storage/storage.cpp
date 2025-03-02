7b1882dfc494103deb65ff28cbc24641f5daa6b0
David Hebbeker <david@hebbeker.info>
2025-03-02 18:42:19 +0100

    Move storage functions to class

--- lib/storage/storage.cpp
+++ lib/storage/storage.cpp
@@ -1,35 +1,159 @@
 #include "storage.hpp"
+#include <FFat.h>
+#include <USB.h>
+#include <USBMSC.h>
+#include <cstdint>
 #include <iostream>
+#include <string>
+
+#if defined(ARDUINO_USB_MODE)
+static_assert(ARDUINO_USB_MODE == 0, "USB must be in OTG mode");
+#endif
 
 const esp_partition_t *check_ffat_partition(const char *label); // defined in FFat.cpp
 
-Storage::Storage(const bool formatFsOnFail, const char *const partitionLabel, ESPUSB &usb, fs::F_Fat &fs, const std::size_t blockSize):
-partition(check_ffat_partition(partitionLabel)),
-blockSize(blockSize),
-fs(fs),
-usb(usb)
+static const std::string rootPath = "/";
+static constexpr std::size_t blockSize = 512; // bytes
+
+static const esp_partition_t *partition;
+static USBMSC usbMsc;
+static bool usbIsRunning = false;
+static bool fileSystemIsReady = false;
+
+static void callbackUsbStarted(void *, esp_event_base_t, int32_t event_id, void *)
 {
-  if(!partition){
-    std::cerr << "Error with partition!" << std::endl;
-    return;
-  }
-
-  // initialize file system
-  const auto basePath = rootPath + partitionLabel;
-  constexpr auto maxOpenFiles = 10;
-  if(!fs.begin(formatFsOnFail, basePath.c_str(), maxOpenFiles, partitionLabel)){
-    std::cerr << "File-system initialization failed!" << std::endl;
-    return;
-  }
-  std::cout << "Storage has a size of " << size() << " bytes." << std::endl;
-
-  // subscribe to USB events
-  callbackSubscribers.push_back(this);
-  usb.onEvent(ARDUINO_USB_STARTED_EVENT, callbackUsbStarted_s);
-  usb.onEvent(ARDUINO_USB_STOPPED_EVENT, callbackUsbStopped_s);
+    usbIsRunning = true;
+    Storage::switchToUsbMode();
 }
 
-std::size_t Storage::size() const
+static void callbackUsbStopped(void *, esp_event_base_t, int32_t event_id, void *)
 {
-  return fs.totalBytes();
+    if (!usbIsRunning) /* https://github.com/espressif/arduino-esp32/issues/7228 */
+    {
+        return;
+    }
+    usbIsRunning = false;
+    Storage::switchToApplicationMode();
+}
+
+/**
+ * Callback invoked when received WRITE10 command.
+ *
+ * Process data in buffer to disk's storage.
+ *
+ * @param lba logical block address
+ * @returns the number of written bytes (must be multiple of block size)
+ */
+static std::int32_t usbMsc_onWrite(const std::uint32_t lba, const std::uint32_t offset, std::uint8_t *const buffer,
+                                   const uint32_t bufsize)
+{
+    std::cout << "MSC WRITE: lba: " << lba << ", offset: " << offset << ", bufsize: " << bufsize << std::endl;
+    const std::uint32_t byteOffset = lba * blockSize + offset;
+    ESP_ERROR_CHECK(esp_partition_erase_range(partition, byteOffset, bufsize)); // erase must be called before write
+    ESP_ERROR_CHECK(esp_partition_write(partition, byteOffset, buffer, bufsize));
+    return bufsize;
+}
+
+//  and
+// return
+
+/**
+ * Callback invoked when received READ10 command.
+ *
+ * Copy disk's data to buffer (up to bufsize).
+ *
+ * @param lba logical block address
+ * @returns the number of copied bytes (must be multiple of block size)
+ */
+static std::int32_t usbMsc_onRead(const std::uint32_t lba, const std::uint32_t offset, void *const buffer,
+                                  const std::uint32_t bufsize)
+{
+    std::cout << "MSC READ: lba: " << lba << ", offset: " << offset << ", bufsize: " << bufsize << std::endl;
+    const std::uint32_t byteOffset = lba * blockSize + offset;
+    ESP_ERROR_CHECK(esp_partition_read(partition, byteOffset, buffer, bufsize));
+    return bufsize;
+}
+
+static bool usbMsc_onStartStop(const std::uint8_t power_condition, const bool start, const bool load_eject)
+{
+    std::cout << "MSC START/STOP: power: " << power_condition << ", start: " << start << ", eject: " << load_eject
+              << std::endl;
+    return true;
+}
+
+std::size_t Storage::size()
+{
+    return FFat.totalBytes();
+}
+
+void Storage::begin(const bool formatFsOnFail, const char *const partitionLabel)
+{
+    partition = check_ffat_partition(partitionLabel);
+
+    if (!partition)
+    {
+        std::cerr << "Error with partition!" << std::endl;
+        return;
+    }
+
+    // initialize file system
+    const auto basePath = rootPath + partitionLabel;
+    constexpr auto maxOpenFiles = 10U;
+    if (!FFat.begin(formatFsOnFail, basePath.c_str(), maxOpenFiles, partitionLabel))
+    {
+        std::cerr << "File-system initialization failed!" << std::endl;
+        return;
+    }
+    std::cout << "Storage has a size of " << size() << " bytes." << std::endl;
+
+    // setup USB Mass Storage Class
+    usbMsc.vendorID("TTS");        // max 8 chars
+    usbMsc.productID("TTD");       // max 16 chars
+    usbMsc.productRevision("1.0"); // max 4 chars
+    usbMsc.onStartStop(usbMsc_onStartStop);
+    // Set callback
+    usbMsc.onRead(usbMsc_onRead);
+    usbMsc.onWrite(usbMsc_onWrite);
+    // usbMsc is ready for read/write
+    usbMsc.mediaPresent(true);
+    usbMsc.begin(FFat.totalBytes() / blockSize, blockSize);
+    fileSystemIsReady = true;
+
+    // subscribe to USB events
+    USB.onEvent(ARDUINO_USB_STARTED_EVENT, callbackUsbStarted);
+    USB.onEvent(ARDUINO_USB_STOPPED_EVENT, callbackUsbStopped);
+    USB.begin();
+}
+
+void Storage::end()
+{
+    usbMsc.end();
+    usbIsRunning = false;
+    FFat.end();
+    fileSystemIsReady = false;
+}
+
+void Storage::switchToUsbMode()
+{
+    FFat.end(); // flush and unmount
+    fileSystemIsReady = false;
+    usbMsc.mediaPresent(true);
+}
+
+bool Storage::isFileSystemReady()
+{
+    return fileSystemIsReady;
+}
+
+void Storage::switchToApplicationMode()
+{
+    usbMsc.mediaPresent(false);
+    FFat.end();   // invalidate cache
+    FFat.begin(); // update data
+    fileSystemIsReady = true;
+}
+
+fs::FS &Storage::getFileSystem()
+{
+    return FFat;
 }
--- lib/storage/storage.hpp
+++ lib/storage/storage.hpp
@@ -1,42 +1,30 @@
 #pragma once
 #include <FFat.h>
-#include <USB.h>
+#include <FS.h>
 #include <USBMSC.h>
 #include <cstddef>
+#include <esp_event_base.h>
 #include <esp_partition.h>
-#include <optional>
 #include <string>
-#include <vector>
 
 class Storage
 {
   public:
-    Storage(bool formatFsOnFail = false, const char *partitionLabel = FFAT_PARTITION_LABEL, ESPUSB &usb = USB,
-            fs::F_Fat &fs = FFat, std::size_t blockSize = 512);
-    bool isFileSystemReady() const;
-    void setCallbackFsReady();
-    void setCallbackFsBusy();
-    std::size_t size() const;
-    std::string getFileContents(const char *path) const;
-    ~Storage();
+    static void begin(bool formatFsOnFail = false, const char *partitionLabel = FFAT_PARTITION_LABEL);
+    static void end();
+    static bool isFileSystemReady();
+    static std::size_t size();
+    static std::string getFileContent(const char *path);
 
-    static constexpr std::string rootPath = "/";
+    /**
+     * Switch from application mode (file system) to USB MSC.
+     */
+    static void switchToUsbMode();
+    /**
+     * Switch from USB MSC to application mode (file system).
+     */
+    static void switchToApplicationMode();
 
   protected:
-    fs::FS &getFileSystem();
-
-  private:
-    const esp_partition_t *partition;
-    /**
-     * Interface to USB mass storage class.
-     */
-    USBMSC usbMsc;
-    const std::size_t blockSize;
-    fs::F_Fat &fs;
-    ESPUSB &usb;
-    static std::vector<Storage *> callbackSubscribers;
-    static esp_event_handler_t callbackUsbStarted_s;
-    void callbackUsbStarted();
-    static esp_event_handler_t callbackUsbStopped_s;
-    void callbackUsbStopped();
+    static fs::FS &getFileSystem();
 };
